c303fdb0445a899e7df4e2c0de2f4175c6361553
Jason Morrison <jmorrison@thoughtbot.com>
2010-11-09 11:32:55 -0500

    Update config/initializers/time_formats.rb for Rails 3

--- template/files/time_formats.rb
+++ template/files/time_formats.rb
@@ -1,5 +1,6 @@
-#{ :short_date  => "%x",              # 04/13/10
-#  :long_date   => "%a, %b %d, %Y"   # Tue, Apr 13, 2010
-#}.each do |k, v|
-#  ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.update(k => v)
-#end
+# { 
+#  :short_date  => "%x",              # 04/13/10
+#  :long_date   => "%a, %b %d, %Y"    # Tue, Apr 13, 2010
+# }.each do |format_name, format_string|
+#   Time::DATE_FORMATS[format_name] = format_string
+# end
