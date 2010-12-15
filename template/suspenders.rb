180abb01c347cdac2591980f0c1abf9f5455544a
Joe Ferris <joe.r.ferris@gmail.com>
2010-12-15 13:32:51 -0500

    Fixed origin url in template

--- template/suspenders.rb
+++ template/suspenders.rb
@@ -42,7 +42,7 @@ def download_file(uri_string, destination)
 end
 
 def origin
-  "git://github.com/thoughtbot/suspenders-gem.git"
+  "git://github.com/thoughtbot/suspenders.git"
 end
 
 def trout(destination_path)
