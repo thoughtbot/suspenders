397675a24e88efbacffe306bde25c0c831a6dc81
Gabe Berke-Williams <gbw@brandeis.edu>
2011-07-16 19:35:15 -0400

    Refactor !blank? to present?

--- template/suspenders.rb
+++ template/suspenders.rb
@@ -43,7 +43,7 @@ def download_file(uri_string, destination)
 end
 
 def origin
-  if !ENV['REPO'].blank?
+  if ENV['REPO'].present?
     ENV['REPO']
   else
     "git://github.com/thoughtbot/suspenders.git"
