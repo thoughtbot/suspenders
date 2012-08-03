4e03d4e69e4557b6a94d819e54c40e358a0bf1c7
Dan Croak <dcroak@thoughtbot.com>
2012-08-02 23:32:29 -0400

    Use consistent formatting
    * Previously, we had a mix of single quotes and double quotes. Use
      single quotes unless interpolating.
    * Previously, we had a mix of dropping parens and using parens when
      invoking methods without a return value. Drop parens unless returning.

--- lib/suspenders/app_builder.rb
+++ lib/suspenders/app_builder.rb
@@ -3,7 +3,7 @@ module Suspenders
     include Suspenders::Actions
 
     def readme
-      template "README.md.erb", "README.md"
+      template 'README.md.erb', 'README.md'
     end
 
     def remove_public_index
@@ -15,56 +15,58 @@ module Suspenders
     end
 
     def raise_delivery_errors
-      replace_in_file "config/environments/development.rb", "raise_delivery_errors = false", "raise_delivery_errors = true"
+      replace_in_file 'config/environments/development.rb',
+        'raise_delivery_errors = false', 'raise_delivery_errors = true'
     end
 
     def enable_factory_girl_syntax
-      copy_file "factory_girl_syntax_rspec.rb", "spec/support/factory_girl.rb"
-      copy_file "factory_girl_syntax_cucumber.rb", "features/support/factory_girl.rb"
+      copy_file 'factory_girl_syntax_rspec.rb', 'spec/support/factory_girl.rb'
+      copy_file 'factory_girl_syntax_cucumber.rb', 'features/support/factory_girl.rb'
     end
 
     def setup_staging_environment
-      run "cp config/environments/production.rb config/environments/staging.rb"
+      run 'cp config/environments/production.rb config/environments/staging.rb'
     end
 
     def create_partials_directory
-      empty_directory "app/views/application"
+      empty_directory 'app/views/application'
     end
 
     def create_shared_flashes
-      copy_file "_flashes.html.erb", "app/views/application/_flashes.html.erb"
+      copy_file '_flashes.html.erb', 'app/views/application/_flashes.html.erb'
     end
 
     def create_shared_javascripts
-      copy_file "_javascript.html.erb", "app/views/application/_javascript.html.erb"
+      copy_file '_javascript.html.erb', 'app/views/application/_javascript.html.erb'
     end
 
     def create_application_layout
-      template "suspenders_layout.html.erb.erb",
-               "app/views/layouts/application.html.erb",
-               :force => true
+      template 'suspenders_layout.html.erb.erb',
+        'app/views/layouts/application.html.erb',
+        :force => true
     end
 
     def create_common_javascripts
-      directory "javascripts", "app/assets/javascripts"
+      directory 'javascripts', 'app/assets/javascripts'
     end
 
     def add_jquery_ui
-      inject_into_file "app/assets/javascripts/application.js", "//= require jquery-ui\n", :before => "//= require_tree ."
+      inject_into_file 'app/assets/javascripts/application.js',
+        '//= require jquery-ui\n', :before => '//= require_tree .'
     end
 
     def use_postgres_config_template
-      template "postgresql_database.yml.erb", "config/database.yml", :force => true
+      template 'postgresql_database.yml.erb', 'config/database.yml', :force => true
     end
 
     def create_database
-      bundle_command('exec rake db:create')
+      bundle_command 'exec rake db:create'
     end
 
     def include_custom_gems
       additions_path = find_in_source_paths 'Gemfile_additions'
       new_gems = File.open(additions_path).read
-      inject_into_file("Gemfile", "\n#{new_gems}", :after => /gem 'jquery-rails'/)
+      inject_into_file 'Gemfile', "\n#{new_gems}", :after => /gem 'jquery-rails'/
     end
 
     def configure_rspec
@@ -73,68 +75,70 @@ module Suspenders
             generate.test_framework :rspec
           end
       RUBY
-      inject_into_class "config/application.rb", "Application", generators_config
+      inject_into_class 'config/application.rb', 'Application', generators_config
     end
 
     def configure_action_mailer
-      action_mailer_host "development", "#{app_name}.local"
-      action_mailer_host "test",        "example.com"
-      action_mailer_host "staging",     "staging.#{app_name}.com"
-      action_mailer_host "production",  "#{app_name}.com"
+      action_mailer_host 'development', "#{app_name}.local"
+      action_mailer_host 'test', 'example.com'
+      action_mailer_host 'staging', "staging.#{app_name}.com"
+      action_mailer_host 'production', "#{app_name}.com"
     end
 
     def generate_rspec
-      generate "rspec:install"
-      replace_in_file "spec/spec_helper.rb", "# config.mock_with :mocha", "config.mock_with :mocha"
+      generate 'rspec:install'
+      replace_in_file 'spec/spec_helper.rb',
+        '# config.mock_with :mocha', 'config.mock_with :mocha'
     end
 
     def generate_cucumber
-      generate "cucumber:install", "--rspec", "--capybara"
-      inject_into_file "features/support/env.rb",
-                       %{Capybara.save_and_open_page_path = 'tmp'\n} +
-                       %{Capybara.javascript_driver = :webkit\n},
-                       :before => %{Capybara.default_selector = :css}
-      inject_into_file 'config/cucumber.yml', ' -r features', :after => %{default: <%= std_opts %> features}
+      generate 'cucumber:install', '--rspec', '--capybara'
+      inject_into_file 'features/support/env.rb',
+        %{Capybara.save_and_open_page_path = 'tmp'\n} +
+        %{Capybara.javascript_driver = :webkit\n},
+        :before => %{Capybara.default_selector = :css}
+      inject_into_file 'config/cucumber.yml',
+        ' -r features', :after => %{default: <%= std_opts %> features}
     end
 
     def generate_clearance
-      generate "clearance:install"
-      generate "clearance:features"
+      generate 'clearance:install'
+      generate 'clearance:features'
     end
 
     def install_factory_girl_steps
-      copy_file "factory_girl_steps.rb", "features/step_definitions/factory_girl_steps.rb"
+      copy_file 'factory_girl_steps.rb', 'features/step_definitions/factory_girl_steps.rb'
     end
 
     def setup_stylesheets
-      copy_file "app/assets/stylesheets/application.css", "app/assets/stylesheets/application.css.scss"
-      remove_file "app/assets/stylesheets/application.css"
-      concat_file "import_scss_styles", "app/assets/stylesheets/application.css.scss"
-      create_file "app/assets/stylesheets/_screen.scss"
+      copy_file 'app/assets/stylesheets/application.css', 'app/assets/stylesheets/application.css.scss'
+      remove_file 'app/assets/stylesheets/application.css'
+      concat_file 'import_scss_styles', 'app/assets/stylesheets/application.css.scss'
+      create_file 'app/assets/stylesheets/_screen.scss'
     end
 
     def gitignore_files
-      concat_file "suspenders_gitignore", ".gitignore"
-      ["app/models",
-        "app/assets/images",
-        "app/views/pages",
-        "db/migrate",
-        "log",
-        "spec/support",
-        "spec/lib",
-        "spec/models",
-        "spec/views",
-        "spec/controllers",
-        "spec/helpers",
-        "spec/support/matchers",
-        "spec/support/mixins",
-        "spec/support/shared_examples"].each do |dir|
+      concat_file 'suspenders_gitignore', '.gitignore'
+      ['app/models',
+        'app/assets/images',
+        'app/views/pages',
+        'db/migrate',
+        'log',
+        'spec/support',
+        'spec/lib',
+        'spec/models',
+        'spec/views',
+        'spec/controllers',
+        'spec/helpers',
+        'spec/support/matchers',
+        'spec/support/mixins',
+        'spec/support/shared_examples'].each do |dir|
         empty_directory_with_gitkeep dir
       end
     end
 
     def init_git
-      run "git init"
+      run 'git init'
     end
 
     def create_heroku_apps
@@ -150,25 +154,25 @@ module Suspenders
     def document_heroku
       heroku_readme_path = find_in_source_paths 'HEROKU_README.md'
       documentation = File.open(heroku_readme_path).read
-      inject_into_file("README.md", "#{documentation}\n", :before => "Most importantly")
+      inject_into_file('README.md', "#{documentation}\n", :before => 'Most importantly')
     end
 
     def copy_miscellaneous_files
-      copy_file "errors.rb", "config/initializers/errors.rb"
-      copy_file "time_formats.rb", "config/initializers/time_formats.rb"
-      copy_file "Procfile"
+      copy_file 'errors.rb', 'config/initializers/errors.rb'
+      copy_file 'time_formats.rb', 'config/initializers/time_formats.rb'
+      copy_file 'Procfile'
     end
 
     def customize_error_pages
       meta_tags =<<-EOS
-  <meta charset="utf-8" />
-  <meta name="ROBOTS" content="NOODP" />
+  <meta charset='utf-8' />
+  <meta name='ROBOTS' content='NOODP' />
       EOS
       style_tags =<<-EOS
-<link href="/assets/application.css" media="all" rel="stylesheet" type="text/css" />
+<link href='/assets/application.css' media='all' rel='stylesheet' type='text/css' />
       EOS
       %w(500 404 422).each do |page|
-        inject_into_file "public/#{page}.html", meta_tags, :after => "<head>\n"
+        inject_into_file "public/#{page}.html", meta_tags, :after => '<head>\n'
         replace_in_file "public/#{page}.html", /<style.+>.+<\/style>/mi, style_tags.strip
         replace_in_file "public/#{page}.html", /<!--.+-->\n/, ''
       end
@@ -179,27 +183,27 @@ module Suspenders
     end
 
     def set_attr_accessibles_on_user
-      inject_into_file "app/models/user.rb",
-        "  attr_accessible :email, :password\n",
+      inject_into_file 'app/models/user.rb',
+        '  attr_accessible :email, :password\n',
         :after => /include Clearance::User\n/
     end
 
     def add_email_validator
-      copy_file "email_validator.rb", "app/validators/email_validator.rb"
+      copy_file 'email_validator.rb', 'app/validators/email_validator.rb'
     end
 
     def include_clearance_matchers
-      create_file "spec/support/clearance.rb", "require 'clearance/testing'"
+      create_file 'spec/support/clearance.rb', "require 'clearance/testing'"
     end
 
     def setup_default_rake_task
-      append_file "Rakefile" do
-        "task(:default).clear\ntask :default => [:spec, :cucumber]"
+      append_file 'Rakefile' do
+        'task(:default).clear\ntask :default => [:spec, :cucumber]'
       end
     end
 
     def add_clearance_gem
-      inject_into_file("Gemfile", "\ngem 'clearance'", :after => /gem 'jquery-rails'/)
+      inject_into_file 'Gemfile', "\ngem 'clearance'", :after => /gem 'jquery-rails'/
     end
   end
 end
--- lib/suspenders/generators/app_generator.rb
+++ lib/suspenders/generators/app_generator.rb
@@ -3,18 +3,17 @@ require 'rails/generators/rails/app/app_generator'
 
 module Suspenders
   class AppGenerator < Rails::Generators::AppGenerator
-    # let's use postgres by default
-    class_option :database,       :type => :string, :aliases => "-d", :default => "postgresql",
-                                  :desc => "Preconfigure for selected database (options: #{DATABASES.join('/')})"
-    # no Test::Unit by default
-    class_option :skip_test_unit, :type => :boolean, :aliases => "-T", :default => true,
-                                  :desc => "Skip Test::Unit files"
+    class_option :database, :type => :string, :aliases => '-d', :default => 'postgresql',
+      :desc => "Preconfigure for selected database (options: #{DATABASES.join('/')})"
 
-    class_option :heroku, :type => :boolean, :aliases => "-H", :default => false,
-                          :desc => "Create staging and production heroku apps"
+    class_option :skip_test_unit, :type => :boolean, :aliases => '-T', :default => true,
+      :desc => 'Skip Test::Unit files'
 
-    class_option :clearance, :type => :boolean, :aliases => "-C", :default => true,
-                             :desc => "Add the clearance Rails authentication library"
+    class_option :heroku, :type => :boolean, :aliases => '-H', :default => false,
+      :desc => 'Create staging and production heroku apps'
+
+    class_option :clearance, :type => :boolean, :aliases => '-C', :default => true,
+      :desc => 'Add the clearance Rails authentication library'
 
     def finish_template
       invoke :suspenders_customization
@@ -41,123 +40,123 @@ module Suspenders
     end
 
     def remove_files_we_dont_need
-      build(:remove_public_index)
-      build(:remove_rails_logo_image)
+      build :remove_public_index
+      build :remove_rails_logo_image
     end
 
     def setup_development_environment
-      say "Setting up the development environment"
-      build(:raise_delivery_errors)
-      build(:enable_factory_girl_syntax)
+      say 'Setting up the development environment'
+      build :raise_delivery_errors
+      build :enable_factory_girl_syntax
     end
 
     def setup_staging_environment
-      say "Setting up the staging environment"
-      build(:setup_staging_environment)
+      say 'Setting up the staging environment'
+      build :setup_staging_environment
     end
 
     def create_suspenders_views
-      say "Creating suspenders views"
-      build(:create_partials_directory)
-      build(:create_shared_flashes)
-      build(:create_shared_javascripts)
-      build(:create_application_layout)
+      say 'Creating suspenders views'
+      build :create_partials_directory
+      build :create_shared_flashes
+      build :create_shared_javascripts
+      build :create_application_layout
     end
 
     def create_common_javascripts
-      say "Pulling in some common javascripts"
-      build(:create_common_javascripts)
+      say 'Pulling in some common javascripts'
+      build :create_common_javascripts
     end
 
     def add_jquery_ui
-      say "Add jQuery ui to the standard application.js"
-      build(:add_jquery_ui)
+      say 'Add jQuery ui to the standard application.js'
+      build :add_jquery_ui
     end
 
     def customize_gemfile
-      build(:include_custom_gems)
+      build :include_custom_gems
       if options[:clearance]
-        build(:add_clearance_gem)
+        build :add_clearance_gem
       end
-      bundle_command('install')
+      bundle_command 'install'
     end
 
     def setup_database
-      say "Setting up database"
+      say 'Setting up database'
       if 'postgresql' == options[:database]
-        build(:use_postgres_config_template)
+        build :use_postgres_config_template
       end
-      build(:create_database)
+      build :create_database
     end
 
     def configure_app
-      say "Configuring app"
-      build(:configure_rspec)
-      build(:configure_action_mailer)
-      build(:generate_rspec)
-      build(:generate_cucumber)
-      build(:install_factory_girl_steps)
-      build(:add_email_validator)
-      build(:setup_default_rake_task)
-      build(:setup_clearance)
+      say 'Configuring app'
+      build :configure_rspec
+      build :configure_action_mailer
+      build :generate_rspec
+      build :generate_cucumber
+      build :install_factory_girl_steps
+      build :add_email_validator
+      build :setup_default_rake_task
+      build :setup_clearance
     end
 
     def setup_clearance
       if options[:clearance]
-        build(:generate_clearance)
-        build(:include_clearance_matchers)
+        build :generate_clearance
+        build :include_clearance_matchers
         if using_active_record?
-          build(:set_attr_accessibles_on_user)
+          build :set_attr_accessibles_on_user
         end
       end
     end
 
     def setup_stylesheets
-      say "Set up stylesheets"
-      build(:setup_stylesheets)
+      say 'Set up stylesheets'
+      build :setup_stylesheets
     end
 
     def setup_git
-      say "Initializing git"
+      say 'Initializing git'
       invoke :setup_gitignore
       invoke :init_git
     end
 
     def create_heroku_apps
       if options['heroku']
-        say "Creating heroku apps"
-        build(:create_heroku_apps)
-        build(:document_heroku)
+        say 'Creating heroku apps'
+        build :create_heroku_apps
+        build :document_heroku
       end
     end
 
     def setup_gitignore
-      build(:gitignore_files)
+      build :gitignore_files
     end
 
     def init_git
-      build(:init_git)
+      build :init_git
     end
 
     def copy_miscellaneous_files
-      say "Copying miscellaneous support files"
-      build(:copy_miscellaneous_files)
+      say 'Copying miscellaneous support files'
+      build :copy_miscellaneous_files
     end
 
     def customize_error_pages
-      say "Customizing the 500/404/422 pages"
-      build(:customize_error_pages)
+      say 'Customizing the 500/404/422 pages'
+      build :customize_error_pages
     end
 
     def setup_root_route
       if options[:clearance]
-        say "Setting up a root route"
-        build(:setup_root_route)
+        say 'Setting up a root route'
+        build :setup_root_route
       end
     end
 
     def outro
-      say "Congratulations! You just pulled our suspenders."
+      say 'Congratulations! You just pulled our suspenders.'
       say "Remember to run 'rails generate airbrake' with your API key."
     end
 
