e9915a89241d5d18d0c7c8e573cbe3cfb9dac243
Geoff Harcourt <geoff.harcourt@gmail.com>
2013-03-08 10:45:34 -0500

    Include factory_girl_rails in development env
    Including FactoryGirl in the development environment ensures that
    factories get created when default generators are used. Adding
    FactoryGirl to development also provides factory functionality when in
    the development console.

--- templates/factories_spec.rb
+++ templates/factories_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 
-FactoryGirl.factories.map(&:name).each do |factory_name|
-  describe "the #{factory_name} factory" do
-    it 'is valid' do
-      expect(build(factory_name)).to be_valid
+describe 'validate FactoryGirl factories' do
+  FactoryGirl.factories.each do |factory|
+    context "with factory for :#{factory.name}" do
+      subject { FactoryGirl.build(factory.name) }
+
+      it 'is valid' do
+        is_valid = subject.valid?
+        expect(is_valid).to be_true, subject.errors.full_messages.join(',')
+      end
     end
   end
 end
