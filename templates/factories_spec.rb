6329c537069b584a62f58064ddfc6036ac897773
Geoff Harcourt <geoff.harcourt@gmail.com>
2013-04-27 15:07:05 -0400

    Use be_valid matcher, avoid use of subject
    Using be_valid matcher rather than awkward be_true check on
    model#valid?. Refactored to avoid explicit use of subject.

--- templates/factories_spec.rb
+++ templates/factories_spec.rb
@@ -3,11 +3,10 @@ require 'spec_helper'
 describe 'validate FactoryGirl factories' do
   FactoryGirl.factories.each do |factory|
     context "with factory for :#{factory.name}" do
-      subject { FactoryGirl.build(factory.name) }
-
       it "is valid" do
-        is_valid = subject.valid?
-        expect(is_valid).to be_true, subject.errors.full_messages.join(',')
+        built_model = build(factory.name)
+
+        expect(built_model).to be_valid
       end
     end
   end
