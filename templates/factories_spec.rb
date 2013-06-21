cd120362cff8e4843985a287f62278a3c152f35a
Arsen Gasparyan <becode21@gmail.com>
2013-06-21 22:48:49 +0400

    Factory must have valid? method

--- templates/factories_spec.rb
+++ templates/factories_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 FactoryGirl.factories.map(&:name).each do |factory_name|
   describe "the #{factory_name} factory" do
     it 'is valid' do
-      expect(build(factory_name)).to be_valid
+      factory = build(factory_name)
+      expect(factory).to be_valid if factory.respond_to?(:valid?)
     end
   end
 end
