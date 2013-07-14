11f44012fc908bd0b0add33312b30104e8ec91ea
Geoff Harcourt <geoff@fivetool.io>
2013-07-14 12:51:13 -0700

    Generators don't generate request or routing specs
    These specs are better written manually as-needed or tested implictly
    via other specs.

--- lib/suspenders/app_builder.rb
+++ lib/suspenders/app_builder.rb
@@ -34,6 +34,8 @@ module Suspenders
   config.generators do |generate|
     generate.helper false
     generate.javascript_engine false
+    generate.request_specs false
+    generate.routing_specs false
     generate.stylesheets false
     generate.test_framework :rspec
     generate.view_specs false
