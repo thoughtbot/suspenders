d7a2441743c1426db43238acc80dfeedcc335b23
Caleb Thompson <caleb@calebthompson.io>
2014-01-14 15:48:10 -0500

    Don't run factory specs twice

--- templates/factories_spec_rake_task.rb
+++ templates/factories_spec_rake_task.rb
@@ -1,6 +1,12 @@
 if defined?(RSpec)
-  desc 'Run factory specs.'
+  task(:spec).clear
 
+  desc 'Run all specs'
+  RSpec::Core::RakeTask.new(:spec) do |t|
+    t.rspec_opts = '--tag ~factory'
+  end
+
+  desc 'Run factory specs.'
   RSpec::Core::RakeTask.new(:factory_specs) do |t|
     t.pattern = './spec/models/factories_spec.rb'
   end
