f1e9eb1fe20fca2653380fe93d739270c76467af
Dan Croak <dan@thoughtbot.com>
2014-02-12 22:13:24 -0800

    Fix .ruby-version on Ruby 2.1.0
    * Ruby 2.1.0 has no patchlevel number because they are moving to SemVer.
      https://twitter.com/hsbt/status/415887230318567424
    * The .ruby-version was leaving a stray `-p` at the end of the number.
      https://github.com/thoughtbot/suspenders/issues/279

--- spec/features/new_project_spec.rb
+++ spec/features/new_project_spec.rb
@@ -16,8 +16,23 @@ feature 'Suspend a new project with default configuration' do
 
     staging_file = IO.read("#{project_path}/config/environments/staging.rb")
     config_stub = "Dummy::Application.configure do"
-
     expect(staging_file).to match(/^require_relative 'production'/)
     expect(staging_file).to match(/#{config_stub}/), staging_file
+  end
+
+  if RUBY_PATCHLEVEL == 0 && RUBY_VERSION >= '2.1.0'
+    scenario '.ruby-version does not include patchlevel for Ruby 2.1.0' do
+      run_suspenders
+
+      ruby_version_file = IO.read("#{project_path}/.ruby-version")
+      expect(ruby_version_file).to eq "2.1.0\n"
+    end
+  else
+    scenario '.ruby-version includes patchlevel for all pre-Ruby 2.1.0 versions' do
+      run_suspenders
+
+      ruby_version_file = IO.read("#{project_path}/.ruby-version")
+      expect(ruby_version_file).to eq "#{RUBY_VERSION}-p#{RUBY_PATCHLEVEL}\n"
+    end
   end
 end
