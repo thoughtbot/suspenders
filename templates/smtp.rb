c528bb63b27ade096efa1a3740542c4a180ecf89
Sean Griffin <sean@thoughtbot.com>
2014-02-16 12:03:03 -0700

    Require mailer config on staging and production
    If we have forgotten to give the smtp config options on staging or
    production, we should know at deploy time, and not have to waste time
    tracking down why emails aren't sending.
    
    This will raise an error when Rails tries to boot without the ENV
    variables. On Heroku staging and production, dynos will crash
    until the config variables are added (and the dynos restarted, which
    happens automatically when config variables are added).

--- templates/smtp.rb
+++ templates/smtp.rb
@@ -1,10 +1,10 @@
 if Rails.env.staging? || Rails.env.production?
   SMTP_SETTINGS = {
-    address: ENV['SMTP_ADDRESS'], # example: 'smtp.sendgrid.net'
+    address: ENV.fetch('SMTP_ADDRESS'), # example: 'smtp.sendgrid.net'
     authentication: :plain,
-    domain: ENV['SMTP_DOMAIN'], # example: 'this-app.com'
-    password: ENV['SMTP_PASSWORD'],
+    domain: ENV.fetch('SMTP_DOMAIN'), # example: 'this-app.com'
+    password: ENV.fetch('SMTP_PASSWORD'),
     port: '587',
-    user_name: ENV['SMTP_USERNAME']
+    user_name: ENV.fetch('SMTP_USERNAME')
   }
 end
