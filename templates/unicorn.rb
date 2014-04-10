f1a92d922d845756f88a8c449939580ab567732b
Simon Taranto <simon@thoughtbot.com>
2014-04-10 09:55:16 -0700

    fix small typo in unicorn.rb

--- templates/unicorn.rb
+++ templates/unicorn.rb
@@ -17,7 +17,7 @@ end
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
   if defined? ActiveRecord::Base
