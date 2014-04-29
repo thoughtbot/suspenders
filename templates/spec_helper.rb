656fc855a83b54b30c10ab5a0097e1c9bca8462c
Lorenzo Masini <l.masini@cantierecreativo.net>
2014-04-29 09:37:26 +0200

    shoulda-matchers spring compatibility.
    Changed Gemfile and spec_helper according to the shoulda-matchers
    documentation:
    
    Gemfile:
    
    group :test do
      gem 'shoulda-matchers', require: false
    end
    
    spec_helper:
    
    require 'rspec/rails'
    require 'shoulda/matchers'u

--- templates/spec_helper.rb
+++ templates/spec_helper.rb
@@ -6,6 +6,7 @@ ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
 require 'rspec/rails'
+require 'shoulda/matchers'
 require 'webmock/rspec'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |file| require file }
