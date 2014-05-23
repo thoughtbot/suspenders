4ac0a6a82e2f0f8ac6ffbe8e13c7aacdff5e0866
Sean Doyle <sean.p.doyle24@gmail.com>
2014-05-23 15:03:45 -0400

    Include `formulaic` gem
    Useful for outside-in integration testing
    
    * integrates nicely with `simple_form`
    * encourages `I18n` from the get-go
    * encourages cleaner `capybara` specs

--- templates/spec_helper.rb
+++ templates/spec_helper.rb
@@ -21,6 +21,8 @@ RSpec.configure do |config|
   end
 
   config.include Features, type: :feature
+  config.include Formulaic::Dsl, type: :feature
+
   config.infer_base_class_for_anonymous_controllers = false
   config.order = 'random'
   config.treat_symbols_as_metadata_keys_with_true_values = true
