64d5f1f01653bd048ec2460d3baa7cc50c36f297
Dan Croak <dan@thoughtbot.com>
2014-05-27 10:30:06 -0700

    Remove before(:suite) block

--- templates/factory_girl_rspec.rb
+++ templates/factory_girl_rspec.rb
@@ -1,11 +1,3 @@
 RSpec.configure do |config|
-  config.before(:suite) do
-    begin
-      DatabaseCleaner.start
-    ensure
-      DatabaseCleaner.clean
-    end
-  end
-
   config.include FactoryGirl::Syntax::Methods
 end
