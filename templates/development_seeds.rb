a20ddf12bc72b981ee2d7103201d611b3a50bcb3
Dan Croak <dan@thoughtbot.com>
2014-07-14 12:46:31 -0700

    Remove unnecessary FactoryGirl.find_definitions
    This line causes a "duplicate factory definition" error when running `rake
    db:setup`, which is run by `bin/setup`.

--- templates/development_seeds.rb
+++ templates/development_seeds.rb
@@ -4,7 +4,6 @@ if Rails.env.development?
   namespace :dev do
     desc "Seed data for development environment"
     task prime: "db:setup" do
-      FactoryGirl.find_definitions
       include FactoryGirl::Syntax::Methods
 
       # create(:user, email: "user@example.com", password: "password")
