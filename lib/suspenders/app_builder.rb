94ec86e60e31463e753537638c97487bc048d285
Jessie A. Young <jessie.a.young@gmail.com>
2014-07-18 15:35:17 -0700

    Single -> Double quotes
    * In line with thoughtbot style guide
    * Adds to https://github.com/thoughtbot/suspenders/commit/6d76dc43c13d13dd66ccbdc8493a6f51f2a2dca6

--- lib/suspenders/app_builder.rb
+++ lib/suspenders/app_builder.rb
@@ -3,12 +3,12 @@ module Suspenders
     include Suspenders::Actions
 
     def readme
-      template 'README.md.erb', 'README.md'
+      template "README.md.erb", "README.md"
     end
 
     def raise_on_delivery_errors
-      replace_in_file 'config/environments/development.rb',
-        'raise_delivery_errors = false', 'raise_delivery_errors = true'
+      replace_in_file "config/environments/development.rb",
+        "raise_delivery_errors = false", "raise_delivery_errors = true"
     end
 
     def raise_on_unpermitted_parameters
@@ -26,12 +26,12 @@ module Suspenders
     end
 
     def provide_setup_script
-      copy_file 'bin_setup', 'bin/setup'
-      run 'chmod a+x bin/setup'
+      copy_file "bin_setup", "bin/setup"
+      run "chmod a+x bin/setup"
     end
 
     def provide_dev_prime_task
-      copy_file 'development_seeds.rb', 'lib/tasks/development_seeds.rake'
+      copy_file "development_seeds.rb", "lib/tasks/development_seeds.rake"
     end
 
     def configure_generators
@@ -48,21 +48,21 @@ module Suspenders
 
       RUBY
 
-      inject_into_class 'config/application.rb', 'Application', config
+      inject_into_class "config/application.rb", "Application", config
     end
 
     def set_up_factory_girl_for_rspec
-      copy_file 'factory_girl_rspec.rb', 'spec/support/factory_girl.rb'
+      copy_file "factory_girl_rspec.rb", "spec/support/factory_girl.rb"
     end
 
     def configure_newrelic
-      template 'newrelic.yml.erb', 'config/newrelic.yml'
+      template "newrelic.yml.erb", "config/newrelic.yml"
     end
 
     def configure_smtp
-      copy_file 'smtp.rb', 'config/smtp.rb'
+      copy_file "smtp.rb", "config/smtp.rb"
 
-      prepend_file 'config/environments/production.rb',
+      prepend_file "config/environments/production.rb",
         %{require Rails.root.join("config/smtp")\n}
 
       config = <<-RUBY
@@ -71,8 +71,8 @@ module Suspenders
   config.action_mailer.smtp_settings = SMTP_SETTINGS
       RUBY
 
-      inject_into_file 'config/environments/production.rb', config,
-        :after => 'config.action_mailer.raise_delivery_errors = false'
+      inject_into_file "config/environments/production.rb", config,
+        :after => "config.action_mailer.raise_delivery_errors = false"
     end
 
     def enable_rack_deflater
@@ -82,27 +82,27 @@ module Suspenders
   config.middleware.use Rack::Deflater
       RUBY
 
-      inject_into_file 'config/environments/production.rb', config,
+      inject_into_file "config/environments/production.rb", config,
         :after => "config.serve_static_assets = false\n"
     end
 
     def setup_asset_host
-      replace_in_file 'config/environments/production.rb',
+      replace_in_file "config/environments/production.rb",
         '# config.action_controller.asset_host = "http://assets.example.com"',
         'config.action_controller.asset_host = ENV.fetch("ASSET_HOST")'
 
-      replace_in_file 'config/initializers/assets.rb',
+      replace_in_file "config/initializers/assets.rb",
         "config.assets.version = '1.0'",
         'config.assets.version = (ENV["ASSETS_VERSION"] || "1.0")'
 
-      replace_in_file 'config/environments/production.rb',
-        'config.serve_static_assets = false',
+      replace_in_file "config/environments/production.rb",
+        "config.serve_static_assets = false",
         'config.static_cache_control = "public, max-age=#{1.year.to_i}"'
     end
 
     def setup_staging_environment
-      staging_file = 'config/environments/staging.rb'
-      copy_file 'staging.rb', staging_file
+      staging_file = "config/environments/staging.rb"
+      copy_file "staging.rb", staging_file
 
       config = <<-RUBY
 
@@ -115,76 +115,76 @@ end
     end
 
     def setup_secret_token
-      template 'secrets.yml', 'config/secrets.yml', force: true
+      template "secrets.yml", "config/secrets.yml", force: true
     end
 
     def create_partials_directory
-      empty_directory 'app/views/application'
+      empty_directory "app/views/application"
     end
 
     def create_shared_flashes
-      copy_file '_flashes.html.erb', 'app/views/application/_flashes.html.erb'
+      copy_file "_flashes.html.erb", "app/views/application/_flashes.html.erb"
     end
 
     def create_shared_javascripts
-      copy_file '_javascript.html.erb', 'app/views/application/_javascript.html.erb'
+      copy_file "_javascript.html.erb", "app/views/application/_javascript.html.erb"
     end
 
     def create_application_layout
-      template 'suspenders_layout.html.erb.erb',
-        'app/views/layouts/application.html.erb',
+      template "suspenders_layout.html.erb.erb",
+        "app/views/layouts/application.html.erb",
         force: true
     end
 
     def remove_turbolinks
-      replace_in_file 'app/assets/javascripts/application.js',
+      replace_in_file "app/assets/javascripts/application.js",
         /\/\/= require turbolinks\n/,
         ''
     end
 
     def use_postgres_config_template
-      template 'postgresql_database.yml.erb', 'config/database.yml',
+      template "postgresql_database.yml.erb", "config/database.yml",
         force: true
     end
 
     def create_database
-      bundle_command 'exec rake db:create db:migrate'
+      bundle_command "exec rake db:create db:migrate"
     end
 
     def replace_gemfile
-      remove_file 'Gemfile'
-      template 'Gemfile.erb', 'Gemfile'
+      remove_file "Gemfile"
+      template "Gemfile.erb", "Gemfile"
     end
 
     def set_ruby_to_version_being_used
-      create_file '.ruby-version', "#{Suspenders::RUBY_VERSION}\n"
+      create_file ".ruby-version", "#{Suspenders::RUBY_VERSION}\n"
     end
 
     def setup_heroku_specific_gems
-      inject_into_file 'Gemfile', "\n\s\sgem 'rails_12factor'",
+      inject_into_file "Gemfile", "\n\s\sgem 'rails_12factor'",
         after: /group :staging, :production do/
     end
 
     def enable_database_cleaner
-      copy_file 'database_cleaner_rspec.rb', 'spec/support/database_cleaner.rb'
+      copy_file "database_cleaner_rspec.rb", "spec/support/database_cleaner.rb"
     end
 
     def configure_spec_support_features
-      empty_directory_with_keep_file 'spec/features'
-      empty_directory_with_keep_file 'spec/support/features'
+      empty_directory_with_keep_file "spec/features"
+      empty_directory_with_keep_file "spec/support/features"
     end
 
     def configure_rspec
-      remove_file 'spec/spec_helper.rb'
-      copy_file 'spec_helper.rb', 'spec/spec_helper.rb'
+      remove_file "spec/spec_helper.rb"
+      copy_file "spec_helper.rb", "spec/spec_helper.rb"
     end
 
     def configure_travis
-      template 'travis.yml.erb', '.travis.yml'
+      template "travis.yml.erb", ".travis.yml"
     end
 
     def configure_i18n_for_test_environment
-      copy_file 'i18n.rb', 'spec/support/i18n.rb'
+      copy_file "i18n.rb", "spec/support/i18n.rb"
       raise_on_missing_translations_in("test")
     end
 
@@ -193,12 +193,12 @@ end
     end
 
     def configure_background_jobs_for_rspec
-      copy_file 'background_jobs_rspec.rb', 'spec/support/background_jobs.rb'
-      run 'rails g delayed_job:active_record'
+      copy_file "background_jobs_rspec.rb", "spec/support/background_jobs.rb"
+      run "rails g delayed_job:active_record"
     end
 
     def configure_action_mailer_in_specs
-      copy_file 'action_mailer.rb', 'spec/support/action_mailer.rb'
+      copy_file "action_mailer.rb", "spec/support/action_mailer.rb"
     end
 
     def configure_time_zone
@@ -206,23 +206,23 @@ end
     config.active_record.default_timezone = :utc
 
       RUBY
-      inject_into_class 'config/application.rb', 'Application', config
+      inject_into_class "config/application.rb", "Application", config
     end
 
     def configure_time_formats
-      remove_file 'config/locales/en.yml'
-      copy_file 'config_locales_en.yml', 'config/locales/en.yml'
+      remove_file "config/locales/en.yml"
+      copy_file "config_locales_en.yml", "config/locales/en.yml"
     end
 
     def configure_rack_timeout
-      copy_file 'rack_timeout.rb', 'config/initializers/rack_timeout.rb'
+      copy_file "rack_timeout.rb", "config/initializers/rack_timeout.rb"
     end
 
     def configure_action_mailer
-      action_mailer_host 'development', "#{app_name}.local"
-      action_mailer_host 'test', 'www.example.com'
-      action_mailer_host 'staging', "staging.#{app_name}.com"
-      action_mailer_host 'production', "#{app_name}.com"
+      action_mailer_host "development", "#{app_name}.local"
+      action_mailer_host "test", "www.example.com"
+      action_mailer_host "staging", "staging.#{app_name}.com"
+      action_mailer_host "production", "#{app_name}.com"
     end
 
     def fix_i18n_deprecation_warning
@@ -230,26 +230,26 @@ end
     config.i18n.enforce_available_locales = true
 
       RUBY
-      inject_into_class 'config/application.rb', 'Application', config
+      inject_into_class "config/application.rb", "Application", config
     end
 
     def generate_rspec
-      generate 'rspec:install'
+      generate "rspec:install"
     end
 
     def configure_unicorn
-      copy_file 'unicorn.rb', 'config/unicorn.rb'
+      copy_file "unicorn.rb", "config/unicorn.rb"
     end
 
     def setup_foreman
-      copy_file 'sample.env', '.sample.env'
-      copy_file 'Procfile', 'Procfile'
+      copy_file "sample.env", ".sample.env"
+      copy_file "Procfile", "Procfile"
     end
 
     def setup_stylesheets
-      remove_file 'app/assets/stylesheets/application.css'
-      copy_file 'application.css.scss',
-        'app/assets/stylesheets/application.css.scss'
+      remove_file "app/assets/stylesheets/application.css"
+      copy_file "application.css.scss",
+        "app/assets/stylesheets/application.css.scss"
     end
 
     def install_bitters
@@ -257,16 +257,16 @@ end
     end
 
     def gitignore_files
-      remove_file '.gitignore'
-      copy_file 'suspenders_gitignore', '.gitignore'
+      remove_file ".gitignore"
+      copy_file "suspenders_gitignore", ".gitignore"
       [
-        'app/views/pages',
-        'spec/lib',
-        'spec/controllers',
-        'spec/helpers',
-        'spec/support/matchers',
-        'spec/support/mixins',
-        'spec/support/shared_examples'
+        "app/views/pages",
+        "spec/lib",
+        "spec/controllers",
+        "spec/helpers",
+        "spec/support/matchers",
+        "spec/support/mixins",
+        "spec/support/shared_examples"
       ].each do |dir|
         run "mkdir #{dir}"
         run "touch #{dir}/.keep"
@@ -274,7 +274,7 @@ end
     end
 
     def init_git
-      run 'git init'
+      run "git init"
     end
 
     def create_heroku_apps
@@ -296,7 +296,7 @@ git remote add production git@heroku.com:#{app_name}-production.git || true
 #{join_heroku_app('production')}
       SHELL
 
-      append_file 'bin/setup', remotes
+      append_file "bin/setup", remotes
     end
 
     def join_heroku_app(environment)
@@ -322,12 +322,12 @@ fi
     end
 
     def setup_segment_io
-      copy_file '_analytics.html.erb',
-        'app/views/application/_analytics.html.erb'
+      copy_file "_analytics.html.erb",
+        "app/views/application/_analytics.html.erb"
     end
 
     def copy_miscellaneous_files
-      copy_file 'errors.rb', 'config/initializers/errors.rb'
+      copy_file "errors.rb", "config/initializers/errors.rb"
     end
 
     def customize_error_pages
@@ -344,17 +344,17 @@ fi
     end
 
     def remove_routes_comment_lines
-      replace_in_file 'config/routes.rb',
+      replace_in_file "config/routes.rb",
         /Rails\.application\.routes\.draw do.*end/m,
         "Rails.application.routes.draw do\nend"
     end
 
     def disable_xml_params
-      copy_file 'disable_xml_params.rb', 'config/initializers/disable_xml_params.rb'
+      copy_file "disable_xml_params.rb", "config/initializers/disable_xml_params.rb"
     end
 
     def setup_default_rake_task
-      append_file 'Rakefile' do
+      append_file "Rakefile" do
         "task(:default).clear\ntask default: [:spec]\n"
       end
     end
@@ -362,20 +362,20 @@ fi
     private
 
     def raise_on_missing_translations_in(environment)
-      config = 'config.action_view.raise_on_missing_translations = true'
+      config = "config.action_view.raise_on_missing_translations = true"
 
       uncomment_in_file("config/environments/#{environment}.rb", config)
     end
 
     def override_path_for_tests
-      if ENV['TESTING']
-        support_bin = File.expand_path(File.join('..', '..', 'spec', 'fakes', 'bin'))
+      if ENV["TESTING"]
+        support_bin = File.expand_path(File.join("..", "..", "spec", "fakes", "bin"))
         "PATH=#{support_bin}:$PATH"
       end
     end
 
     def factories_spec_rake_task
-      IO.read find_in_source_paths('factories_spec_rake_task.rb')
+      IO.read find_in_source_paths("factories_spec_rake_task.rb")
     end
 
     def generate_secret
--- lib/suspenders/generators/app_generator.rb
+++ lib/suspenders/generators/app_generator.rb
@@ -1,19 +1,19 @@
-require 'rails/generators'
-require 'rails/generators/rails/app/app_generator'
+require "rails/generators"
+require "rails/generators/rails/app/app_generator"
 
 module Suspenders
   class AppGenerator < Rails::Generators::AppGenerator
-    class_option :database, :type => :string, :aliases => '-d', :default => 'postgresql',
+    class_option :database, :type => :string, :aliases => "-d", :default => "postgresql",
       :desc => "Preconfigure for selected database (options: #{DATABASES.join('/')})"
 
-    class_option :heroku, :type => :boolean, :aliases => '-H', :default => false,
-      :desc => 'Create staging and production Heroku apps'
+    class_option :heroku, :type => :boolean, :aliases => "-H", :default => false,
+      :desc => "Create staging and production Heroku apps"
 
-    class_option :github, :type => :string, :aliases => '-G', :default => nil,
-      :desc => 'Create Github repository and add remote origin pointed to repo'
+    class_option :github, :type => :string, :aliases => "-G", :default => nil,
+      :desc => "Create Github repository and add remote origin pointed to repo"
 
-    class_option :skip_test_unit, :type => :boolean, :aliases => '-T', :default => true,
-      :desc => 'Skip Test::Unit files'
+    class_option :skip_test_unit, :type => :boolean, :aliases => "-T", :default => true,
+      :desc => "Skip Test::Unit files"
 
     def finish_template
       invoke :suspenders_customization
@@ -51,13 +51,13 @@ module Suspenders
         build :setup_heroku_specific_gems
       end
 
-      bundle_command 'install'
+      bundle_command "install"
     end
 
     def setup_database
-      say 'Setting up database'
+      say "Setting up database"
 
-      if 'postgresql' == options[:database]
+      if "postgresql" == options[:database]
         build :use_postgres_config_template
       end
 
@@ -65,7 +65,7 @@ module Suspenders
     end
 
     def setup_development_environment
-      say 'Setting up the development environment'
+      say "Setting up the development environment"
       build :raise_on_delivery_errors
       build :raise_on_unpermitted_parameters
       build :provide_setup_script
@@ -75,7 +75,7 @@ module Suspenders
     end
 
     def setup_test_environment
-      say 'Setting up the test environment'
+      say "Setting up the test environment"
       build :set_up_factory_girl_for_rspec
       build :generate_rspec
       build :configure_rspec
@@ -88,7 +88,7 @@ module Suspenders
     end
 
     def setup_production_environment
-      say 'Setting up the production environment'
+      say "Setting up the production environment"
       build :configure_newrelic
       build :configure_smtp
       build :enable_rack_deflater
@@ -96,17 +96,17 @@ module Suspenders
     end
 
     def setup_staging_environment
-      say 'Setting up the staging environment'
+      say "Setting up the staging environment"
       build :setup_staging_environment
     end
 
     def setup_secret_token
-      say 'Moving secret token out of version control'
+      say "Moving secret token out of version control"
       build :setup_secret_token
     end
 
     def create_suspenders_views
-      say 'Creating suspenders views'
+      say "Creating suspenders views"
       build :create_partials_directory
       build :create_shared_flashes
       build :create_shared_javascripts
@@ -114,12 +114,12 @@ module Suspenders
     end
 
     def setup_coffeescript
-      say 'Setting up CoffeeScript defaults'
+      say "Setting up CoffeeScript defaults"
       build :remove_turbolinks
     end
 
     def configure_app
-      say 'Configuring app'
+      say "Configuring app"
       build :configure_action_mailer
       build :configure_time_zone
       build :configure_time_formats
@@ -132,18 +132,18 @@ module Suspenders
     end
 
     def setup_stylesheets
-      say 'Set up stylesheets'
+      say "Set up stylesheets"
       build :setup_stylesheets
     end
 
     def install_bitters
-      say 'Install Bitters'
+      say "Install Bitters"
       build :install_bitters
     end
 
     def setup_git
       if !options[:skip_git]
-        say 'Initializing git'
+        say "Initializing git"
         invoke :setup_gitignore
         invoke :init_git
       end
@@ -151,7 +151,7 @@ module Suspenders
 
     def create_heroku_apps
       if options[:heroku]
-        say 'Creating Heroku apps'
+        say "Creating Heroku apps"
         build :create_heroku_apps
         build :set_heroku_remotes
         build :set_heroku_rails_secrets
@@ -160,13 +160,13 @@ module Suspenders
 
     def create_github_repo
       if !options[:skip_git] && options[:github]
-        say 'Creating Github repo'
+        say "Creating Github repo"
         build :create_github_repo, options[:github]
       end
     end
 
     def setup_segment_io
-      say 'Setting up Segment.io'
+      say "Setting up Segment.io"
       build :setup_segment_io
     end
 
@@ -179,12 +179,12 @@ module Suspenders
     end
 
     def copy_miscellaneous_files
-      say 'Copying miscellaneous support files'
+      say "Copying miscellaneous support files"
       build :copy_miscellaneous_files
     end
 
     def customize_error_pages
-      say 'Customizing the 500/404/422 pages'
+      say "Customizing the 500/404/422 pages"
       build :customize_error_pages
     end
 
@@ -193,7 +193,7 @@ module Suspenders
     end
 
     def outro
-      say 'Congratulations! You just pulled our suspenders.'
+      say "Congratulations! You just pulled our suspenders."
       say "Remember to run 'rails generate airbrake' with your API key."
     end
 
