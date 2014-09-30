936dcc70e49a5aa61667e17442f1d7ca6ed30582
Dan Croak <dan@thoughtbot.com>
2014-09-29 21:12:25 -0700

    Generate rails_helper, not spec_helper
    RSpec 3.x introduces a `spec/rails_helper.rb` file
    which contains all dependencies necessary to run specs that need Rails.
    
    https://www.relishapp.com/rspec/rspec-rails/docs/upgrade#default-helper-files
    
    Achieves the goal of "Documenting Explicit Dependencies Through Tests":
    
    http://robots.thoughtbot.com/document-explicit-dependencies-through-tests
    
    * Move Rails-specific things to rails_helper.rb.
    * Require spec_helper.rb from rails_helper.rb.
    * Remove duplication across files.
    * We do not need to require "spec_helper" manually.
      It is required for us in `.rspec`:
    
        --color
        --warnings
        --require spec_helper

--- templates/spec_helper.rb
+++ templates/spec_helper.rb
@@ -1,30 +1,16 @@
-ENV["RAILS_ENV"] = "test"
-
-require File.expand_path("../../config/environment", __FILE__)
-
-require "rspec/rails"
-require "shoulda/matchers"
 require "webmock/rspec"
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |file| require file }
-
-module Features
-  # Extend this module in spec/support/features/*.rb
-  include Formulaic::Dsl
-end
-
+# http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
   end
 
-  config.include Features, type: :feature
-  config.infer_base_class_for_anonymous_controllers = false
-  config.infer_spec_type_from_file_location!
-  config.order = "random"
-  config.use_transactional_fixtures = false
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+  end
+
+  config.order = :random
 end
 
-ActiveRecord::Migration.maintain_test_schema!
-Capybara.javascript_driver = :webkit
 WebMock.disable_net_connect!(allow_localhost: true)
