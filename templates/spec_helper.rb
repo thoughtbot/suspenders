2e9ffd152d1f55b0afe35d0949f9a01c0fd4c9d8
Dan Croak <dan@thoughtbot.com>
2014-09-30 07:53:40 -0700

    Don't add new thoughtbot defaults

--- templates/spec_helper.rb
+++ templates/spec_helper.rb
@@ -6,13 +6,8 @@ RSpec.configure do |config|
     expectations.syntax = :expect
   end
 
-  if config.files_to_run.one?
-    config.default_formatter = "doc"
-  end
-
   config.mock_with :rspec do |mocks|
     mocks.syntax = :expect
-    mocks.verify_partial_doubles = true
   end
 
   config.order = :random
