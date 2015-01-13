744b0c71147e0e00ebcb774fb8c2ff987c78f824
Fabrizio Monti <fabrizio.monti@welaika.com>
2015-01-13 12:47:34 +0100

    Linting factories

--- templates/factory_girl_rspec.rb
+++ templates/factory_girl_rspec.rb
@@ -1,3 +1,12 @@
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    begin
+      DatabaseCleaner.start
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean
+    end
+  end
 end
