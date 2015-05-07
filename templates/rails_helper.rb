ce580ba1040e3c984384815c1329212fa01a1e58
Gabe Berke-Williams <gabebw@gabebw.com>
2015-05-07 17:12:26 -0400

    Require spec/support files in alphabetical order
    This ensures that the `require` order is the same on all machines, including CI.
    
    For example, we mainly use OS X as our local development machines, while CI runs
    on Linux. We've run into ordering issues before due to the different OS's, and
    this changes has historically fixed it.

--- templates/rails_helper.rb
+++ templates/rails_helper.rb
@@ -5,7 +5,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require "rspec/rails"
 require "shoulda/matchers"
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |file| require file }
+Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |file| require file }
 
 module Features
   # Extend this module in spec/support/features/*.rb
