4c8f54f20b1e812a6863d3583556c771c6b813af
Fabrizio Monti <fabrizio.monti@welaika.com>
2016-01-08 16:03:50 +0100

    Change SimpleCov output dir while run in CircleCI
    Following [this guide](https://circleci.com/docs/code-coverage) I was
    able to integrate SimpleCov in CircleCI. This allow us to see
    SimpleCov's HTML report as artifact in CircleCI.
    
    [closes #693]

--- templates/spec_helper.rb
+++ templates/spec_helper.rb
@@ -1,5 +1,11 @@
 if ENV.fetch("COVERAGE", false)
   require "simplecov"
+
+  if ENV["CIRCLE_ARTIFACTS"]
+    dir = File.join(ENV["CIRCLE_ARTIFACTS"], "coverage")
+    SimpleCov.coverage_dir(dir)
+  end
+
   SimpleCov.start "rails"
 end
 
