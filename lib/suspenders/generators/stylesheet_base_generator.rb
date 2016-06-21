6204457e2402da69ce8bce8c84bd30e2e14183dd
Tyson Gach <tyson@thoughtbot.com>
2016-06-21 09:35:28 -0400

    Update Neat to 1.8.0

--- lib/suspenders/generators/stylesheet_base_generator.rb
+++ lib/suspenders/generators/stylesheet_base_generator.rb
@@ -8,7 +8,7 @@ module Suspenders
 
     def add_stylesheet_gems
       gem "bourbon", "5.0.0.beta.6"
-      gem "neat", "~> 1.7.0"
+      gem "neat", "~> 1.8.0"
       gem "refills", group: [:development, :test]
     end
 
