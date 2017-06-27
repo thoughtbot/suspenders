58213a3ffcba58a38e0d82cc5da1e3e1613a2bd3
Fabrizio Monti <fabrizio.monti@welaika.com>
2017-06-27 12:02:28 +0200

    Update Rails to 5.1.2

--- lib/suspenders/version.rb
+++ lib/suspenders/version.rb
@@ -1,5 +1,5 @@
 module Suspenders
-  RAILS_VERSION = "~> 5.1.1".freeze
+  RAILS_VERSION = "~> 5.1.2".freeze
   RUBY_VERSION = IO.
     read("#{File.dirname(__FILE__)}/../../.ruby-version").
     strip.
