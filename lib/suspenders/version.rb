62b133734ca7de8a5070083fa1be090de8764ae9
Dan Moore <dan@vaporwa.re>
2018-03-30 14:40:12 -0400

    Update to Rails 5.1.6, Ruby 2.5.1

--- lib/suspenders/version.rb
+++ lib/suspenders/version.rb
@@ -1,5 +1,5 @@
 module Suspenders
-  RAILS_VERSION = "~> 5.1.4".freeze
+  RAILS_VERSION = "~> 5.1.6".freeze
   RUBY_VERSION = IO.
     read("#{File.dirname(__FILE__)}/../../.ruby-version").
     strip.
