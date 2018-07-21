4f670ac0a1839092ca46d91e36a8e5a5c404b995
Edgar Giovanni Lepe <lepe.edgar10@gmail.com>
2018-07-20 18:30:29 -0700

    Change to double-quoted strings

--- spec/features/database_spec.rb
+++ spec/features/database_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require "spec_helper"
 
-RSpec.shared_context 'Project Setup Hook' do |option|
+RSpec.shared_context "Project Setup Hook" do |option|
   before :context do
     drop_dummy_database
     remove_project_directory
@@ -11,21 +11,21 @@ RSpec.shared_context 'Project Setup Hook' do |option|
   end
 end
 
-RSpec.describe 'Suspend a new project with custom database option' do
-  context '--database=sqlite3' do
-    include_context 'Project Setup Hook', '--database=sqlite3'
+RSpec.describe "Suspend a new project with custom database option" do
+  context "--database=sqlite3" do
+    include_context "Project Setup Hook", "--database=sqlite3"
 
-    it 'adds sqlite3 to Gemfile' do
+    it "adds sqlite3 to Gemfile" do
       gemfile = IO.read "#{project_path}/Gemfile"
 
       expect(gemfile).to match(/sqlite3/)
     end
   end
 
-  context '--database=mysql' do
-    include_context 'Project Setup Hook', '--database=mysql'
+  context "--database=mysql" do
+    include_context "Project Setup Hook", "--database=mysql"
 
-    it 'adds mysql2 to Gemfile' do
+    it "adds mysql2 to Gemfile" do
       gemfile = IO.read "#{project_path}/Gemfile"
 
       expect(gemfile).to match(/mysql2/)
