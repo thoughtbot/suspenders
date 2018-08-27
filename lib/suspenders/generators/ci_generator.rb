72827aeb0723dc0632d7922ee115d0d968f48b46
Mehdi Lahmam <mehdi@lahmam.com>
2018-08-27 18:38:20 +0200

    Remove CircleCI 1.0 artifacts path config for SimpleCov
    This is not necessary anymore with CircleCI 2.0.
    Note also that `CIRCLE_ARTIFACTS` isn't defined in 2.0
    https://circleci.com/docs/2.0/migration/#tips-for-migrating-from-10-to-20
    
    The default directory `coverage` is added to stored artifacts paths so
    it shows up on circleci.com `Artifacts` tab.

--- lib/suspenders/generators/ci_generator.rb
+++ lib/suspenders/generators/ci_generator.rb
@@ -6,19 +6,6 @@ module Suspenders
       File.join("..", "..", "..", "templates"),
       File.dirname(__FILE__))
 
-    def simplecov_test_integration
-      inject_into_file "spec/spec_helper.rb", before: 'SimpleCov.start "rails"' do
-        <<-RUBY
-
-  if ENV["CIRCLE_ARTIFACTS"]
-    dir = File.join(ENV["CIRCLE_ARTIFACTS"], "coverage")
-    SimpleCov.coverage_dir(dir)
-  end
-
-        RUBY
-      end
-    end
-
     def configure_ci
       empty_directory(".circleci")
       template "circleci.yml.erb", ".circleci/config.yml"
