9c6f1d5d952ff01be38472ecfb48b19a9ce9ae5e
Dan Barber <github@danbarber.me>
2018-11-09 14:04:10 -0500

    We need to explicitly set the ASSET_HOST too

--- lib/suspenders/app_builder.rb
+++ lib/suspenders/app_builder.rb
@@ -102,6 +102,7 @@ module Suspenders
       config = <<-RUBY
   if ENV.fetch("HEROKU_APP_NAME", "").include?("staging-pr-")
     ENV["APPLICATION_HOST"] = ENV["HEROKU_APP_NAME"] + ".herokuapp.com"
+    ENV["ASSET_HOST"] = ENV["HEROKU_APP_NAME"] + ".herokuapp.com"
   end
       RUBY
 
