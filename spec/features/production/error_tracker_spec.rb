01c0cf08113c4dcb738695c210a3677a02b17049
Mike Burns <mike@mike-burns.com>
2019-02-27 11:06:42 -0500

    left off here?

--- spec/features/production/error_tracker_spec.rb
+++ spec/features/production/error_tracker_spec.rb
@@ -3,6 +3,8 @@ require "spec_helper"
 RSpec.describe "suspenders:production:error_tracker", type: :generator do
   it "inserts the HONEYBADGER_ENV into Heroku" do
     with_app { generate("suspenders:production:error_tracker") }
+
+    # TODO
   end
 
   it "removes the HONEYBADGER_ENV from Heroku" do
