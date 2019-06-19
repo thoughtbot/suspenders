dd1a9b0a6356675a596cc91cc2269b9d2ca8d076
Gabe Berke-Williams <gabebw@gabebw.com>
2019-06-19 00:12:31 -0700

    Set a bigger window for `save_and_open_screenshot`
    1680x1050 is the size of a 15" retina Macbook Pro.
    
    Without this setting, `save_and_open_screenshot` captures perhaps 75% of
    the screen, which makes it much less useful for debugging.
    
    I'm sure some people have bigger screens, and could use an even larger
    window size, but this feels like a good default.

--- templates/chromedriver.rb
+++ templates/chromedriver.rb
@@ -7,6 +7,7 @@ end
 Capybara.register_driver :headless_chrome do |app|
   options = ::Selenium::WebDriver::Chrome::Options.new
   options.headless!
+  options.add_argument "--window-size=1680,1050"
 
   Capybara::Selenium::Driver.new app,
     browser: :chrome,
