b1d02e7486650ea811dc1d5698f48c82e4d35884
Mike Burns <mburns@thoughtbot.com>
2020-04-24 16:58:31 -0400

    bundler-audit 0.7.0
    Use bundler-audit 0.7.0, currently in pre-release. It has an
    up-to-date Thor dependency, which fixes an incompatibility with Bundler
    2.
    
    Here is a trail of bug reports and fixes against the repo:
    
    * <https://github.com/rubysec/bundler-audit/issues/239>
    * <https://github.com/rubysec/bundler-audit/pull/237>
    * <https://github.com/rubysec/bundler-audit/pull/252>

--- lib/suspenders/generators/advisories_generator.rb
+++ lib/suspenders/generators/advisories_generator.rb
@@ -3,7 +3,11 @@ require_relative "base"
 module Suspenders
   class AdvisoriesGenerator < Generators::Base
     def bundler_audit_gem
-      gem "bundler-audit", require: false, group: %i[development test]
+      gem "bundler-audit",
+          require: false,
+          group: %i[development test],
+          git: "https://github.com/rubysec/bundler-audit.git",
+          branch: "0.7.0"
       Bundler.with_clean_env { run "bundle install" }
     end
 
