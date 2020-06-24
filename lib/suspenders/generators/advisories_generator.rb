d6b0a3c04448a0849a29a48c00944467b6b84bfe
Daniel Colson <danieljamescolson@gmail.com>
2020-06-24 18:32:43 -0400

    Revert "bundler-audit 0.7.0"
    This mostly reverts b1d02e7486650ea811dc1d5698f48c82e4d35884, adding a
    >= 0.7.0 to ensure we can use Thor 2 as described in that commit.
    
    Without this commit this generator creates a broken Gemfile because the
    0.7.0 no longer exists.

--- lib/suspenders/generators/advisories_generator.rb
+++ lib/suspenders/generators/advisories_generator.rb
@@ -3,11 +3,7 @@ require_relative "base"
 module Suspenders
   class AdvisoriesGenerator < Generators::Base
     def bundler_audit_gem
-      gem "bundler-audit",
-          require: false,
-          group: %i[development test],
-          git: "https://github.com/rubysec/bundler-audit.git",
-          branch: "0.7.0"
+      gem "bundler-audit", ">= 0.7.0", require: false, group: %i[development test]
       Bundler.with_clean_env { run "bundle install" }
     end
 
