c00b688b149a75b4f31d67d89263d870b6c17011
Chris Thorn <chris@thorn.co>
2020-07-10 18:06:52 -0400

    Add standard to lint generator
    This commit adds the [standard] gem, and requires the standard rake task
    in Rakefile.
    
    It also disables RuboCop on Hound, since standard is meant to replace
    RuboCop.
    
    [standard]: https://github.com/testdouble/standard
    
    Co-authored-by: Daniel Colson <danieljamescolson@gmail.com>

--- lib/suspenders/generators/lint_generator.rb
+++ lib/suspenders/generators/lint_generator.rb
@@ -5,5 +5,10 @@ module Suspenders
     def set_up_hound
       copy_file "hound.yml", ".hound.yml"
     end
+
+    def set_up_standard
+      gem "standard", group: :development
+      prepend_to_file("Rakefile", 'require "standard/rake"')
+    end
   end
 end
