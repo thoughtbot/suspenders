fce7cda58753cd842e6f7eceb85ab8a480dea36c
Thiago A. Silva <thiagoaraujos@gmail.com>
2021-08-15 20:37:05 -0300

    Improve match_original_file matcher's error message with a diff

--- spec/support/generator_matchers.rb
+++ spec/support/generator_matchers.rb
@@ -1,8 +1,24 @@
 RSpec::Matchers.define :match_original_file do
   match do |path|
-    original_contents = GeneratorTestHelpers.app_template_path.join(path).read
-    actual_contents = GeneratorTestHelpers.app_path.join(path).read
+    @original_contents = GeneratorTestHelpers.app_template_path.join(path).read
+    @actual_contents = GeneratorTestHelpers.app_path.join(path).read
 
-    original_contents == actual_contents
+    @original_contents == @actual_contents
+  end
+
+  failure_message do
+    <<~MESSAGE
+      Gemfile does not match original file. The diff is:
+      #{differ.diff_as_string(@actual_contents, @original_contents)}
+    MESSAGE
+  end
+
+  def differ
+    RSpec::Support::Differ.new(
+      object_preparer: lambda do |object|
+        RSpec::Matchers::Composable.surface_descriptions_in(object)
+      end,
+      color: RSpec::Matchers.configuration.color?
+    )
   end
 end
