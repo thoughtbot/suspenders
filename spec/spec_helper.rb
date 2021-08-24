27d016497f42058c1fc93d8bcf3d09969b7223e1
Thiago A. Silva <thiagoaraujos@gmail.com>
2021-08-24 09:54:14 -0300

    Fix build

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,7 +12,7 @@ RSpec.configure do |config|
 
   config.before(:all) do
     FileOperations.create_tmp_path
-    EnvPath.prepend_env_path!(fake_bin_path)
+    EnvPath.prepend_env_path!(TestPaths.fake_bin_path)
   end
 
   config.before(:each, type: :feature) do |spec|
