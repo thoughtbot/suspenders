862e5a3a4fdc20d05278ffa765981798197ddc84
Thiago A. Silva <thiagoaraujos@gmail.com>
2021-09-04 12:09:42 -0300

    Refactor db optimizations generator spec

--- spec/suspenders/generators/db_optimizations_generator_spec.rb
+++ spec/suspenders/generators/db_optimizations_generator_spec.rb
@@ -1,27 +1,49 @@
 require "spec_helper"
 
 RSpec.describe Suspenders::DbOptimizationsGenerator, type: :generator do
-  it "generates and destroys bullet" do
-    with_fake_app do
-      invoke! Suspenders::DbOptimizationsGenerator
+  describe "invoke" do
+    it "bundles the bullet gem" do
+      with_fake_app do
+        invoke! Suspenders::DbOptimizationsGenerator
 
-      expect("Gemfile")
-        .to have_no_syntax_error
-        .and have_bundled("install")
-        .matching(/bullet/)
-      expect("config/environments/development.rb")
-        .to have_no_syntax_error
-        .and match_contents(/Bullet.enable/)
+        expect("Gemfile")
+          .to have_no_syntax_error
+          .and have_bundled("install")
+          .matching(/bullet/)
+      end
+    end
 
-      revoke! Suspenders::DbOptimizationsGenerator
+    it "configures development.rb with bullet" do
+      with_fake_app do
+        invoke! Suspenders::DbOptimizationsGenerator
 
-      expect("Gemfile")
-        .to have_no_syntax_error
-        .and match_original_file
-        .and not_have_bundled
-      expect("config/environments/development.rb")
-        .to have_no_syntax_error
-        .and match_original_file
+        expect("config/environments/development.rb")
+          .to have_no_syntax_error
+          .and match_contents(/Bullet.enable/)
+      end
+    end
+  end
+
+  describe "revoke" do
+    it "removes the bullet gem from Gemfile" do
+      with_fake_app do
+        invoke_then_revoke! Suspenders::DbOptimizationsGenerator
+
+        expect("Gemfile")
+          .to have_no_syntax_error
+          .and match_original_file
+          .and not_have_bundled
+      end
+    end
+
+    it "removes bullet configuration from development.rb" do
+      with_fake_app do
+        invoke_then_revoke! Suspenders::DbOptimizationsGenerator
+
+        expect("config/environments/development.rb")
+          .to have_no_syntax_error
+          .and match_original_file
+      end
     end
   end
 end
