b41e7062bf1787700389d3365f7b637fe2e712da
Thiago A. Silva <thiagoaraujos@gmail.com>
2021-09-06 10:44:03 -0300

    Get rid of smells in static_generator_spec

--- spec/suspenders/generators/static_generator_spec.rb
+++ spec/suspenders/generators/static_generator_spec.rb
@@ -1,27 +1,45 @@
 require "spec_helper"
 
 RSpec.describe Suspenders::StaticGenerator, type: :generator do
-  it "generates the high_voltage gem and pages directory" do
-    with_fake_app do
-      invoke! Suspenders::StaticGenerator
+  describe "invoke" do
+    it "bundles the high_voltage gem" do
+      with_fake_app do
+        invoke! Suspenders::StaticGenerator
 
-      expect("app/views/pages/.keep").to exist_as_a_file
-      expect("Gemfile")
-        .to have_no_syntax_error
-        .and have_bundled("install")
-        .matching(/high_voltage/)
+        expect("Gemfile")
+          .to have_no_syntax_error
+          .and have_bundled("install")
+          .matching(/high_voltage/)
+      end
+    end
+
+    it "creates a views/pages/.keep file" do
+      with_fake_app do
+        invoke! Suspenders::StaticGenerator
+
+        expect("app/views/pages/.keep").to exist_as_a_file
+      end
     end
   end
 
-  it "destroys the high_voltage gem and pages directory" do
-    with_fake_app do
-      invoke_then_revoke! Suspenders::StaticGenerator
+  describe "revoke" do
+    it "removes the high_voltage gem from Gemfile" do
+      with_fake_app do
+        invoke_then_revoke! Suspenders::StaticGenerator
 
-      expect("app/views/pages/.keep").not_to exist_as_a_file
-      expect("Gemfile")
-        .to have_no_syntax_error
-        .and match_original_file
-        .and not_have_bundled
+        expect("Gemfile")
+          .to have_no_syntax_error
+          .and match_original_file
+          .and not_have_bundled
+      end
+    end
+
+    it "deletes the views/pages/.keep file" do
+      with_fake_app do
+        invoke_then_revoke! Suspenders::StaticGenerator
+
+        expect("app/views/pages/.keep").not_to exist_as_a_file
+      end
     end
   end
 end
