a6dd9e75f78acc34711b3373ebeb310c1e5659b4
Thiago A. Silva <thiagoaraujos@gmail.com>
2021-09-17 13:20:18 -0300

    Clarify names in yarn matcher

--- spec/support/fake_yarn_matchers.rb
+++ spec/support/fake_yarn_matchers.rb
@@ -1,15 +1,15 @@
 # FakeYarn records yarn information through a fake "bin/yarn"
 # binary. This RSpec matcher relies on that functionality.
-RSpec::Matchers.define :have_yarned do |actual_yarn_args = ""|
+RSpec::Matchers.define :have_yarned do |expected_yarn_args = ""|
   match do |file_str|
     raise 'Use expect("package.json")' if file_str != "package.json"
 
     fake_yarn = FakeYarn.new
 
     @yarn_ran = fake_yarn.ran?
-    @given_yarn_args = fake_yarn.arguments
+    @actual_yarn_args = fake_yarn.arguments
 
-    @yarn_ran && @given_yarn_args == actual_yarn_args
+    @yarn_ran && expected_yarn_args == @actual_yarn_args
   end
 
   failure_message do
@@ -18,7 +18,7 @@ RSpec::Matchers.define :have_yarned do |actual_yarn_args = ""|
     else
       <<~MSG
         Yarn was executed, but the given arguments are incorrect!
-        Expected "#{actual_yarn_args}" but got "#{@given_yarn_args}"
+        Expected "#{expected_yarn_args}" but got "#{@actual_yarn_args}"
       MSG
     end
   end
