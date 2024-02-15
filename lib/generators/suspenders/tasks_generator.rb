b7a6d95362bcc809f759fadabd8d5f4220f6517b
Steve Polito <stevepolito@hey.com>
2024-02-15 05:50:31 -0500

    Introduce `suspenders:tasks` generator
    Relates to #1157
    
    For now, this generator simply creates `dev.rake` which contains
    [dev:prime][].
    
    Note that we'll want this generator to run **after**
    `suspenders:factories`.
    
    If future application specific tasks need to be added, we can use this
    generator. However, this should not be confused with the existing
    pattern of creating tasks under the suspenders namespace, such as the
    existing `lib/tasks/suspenders.rake`. Tasks under this namespace cannot
    be edited by the consumer, whereas tasks generated by `suspenders:tasks`
    are intended to be edited by the consumer.
    
    [dev:prime]: https://thoughtbot.com/blog/priming-the-pump

---
+++ lib/generators/suspenders/tasks_generator.rb
@@ -0,0 +1,20 @@
+module Suspenders
+  module Generators
+    class TasksGenerator < Rails::Generators::Base
+      source_root File.expand_path("../../templates/tasks", __FILE__)
+      desc <<~TEXT
+        Creates local Rake tasks for development
+
+        bin/rails dev:prime # Sample data for local development environment
+      TEXT
+
+      def create_dev_rake
+        if Bundler.rubygems.find_name("factory_bot").any?
+          copy_file "dev.rake", "lib/tasks/dev.rake"
+        else
+          say "This generator requires Factory Bot"
+        end
+      end
+    end
+  end
+end
