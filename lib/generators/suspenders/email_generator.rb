19ce9b1ff5efe73f62e3556cd2e6b78dd0a40a8d
Steve Polito <stevepolito@hey.com>
2024-02-23 05:42:34 -0500

    Introduce `suspenders:email` generator
    Creates [email interceptor][] to ensure emails are sent to preconfigured
    addresses in non-production environments. Drops support for [Recipient
    Interceptor][], since this functionality is supported in Rails Core.
    
    In order to enable the interceptor, set `EMAIL_RECIPIENTS` to a
    comma-separated list.
    
    ```sh
    INTERCEPTOR_ADDRESSES="user_1@example.com,user_2@example.com" bin/rails s
    ```
    
    Configures [default_url_options][] in `test` and `development`
    environments so that `*_url` helpers will work.
    
    Note that we do not configure `production`, since we've determined the
    `delivery_method` is something each project will configure
    independently. This means we'll just use the [default configuration][],
    which is `smtp`.
    
    [email interceptor]: https://guides.rubyonrails.org/action_mailer_basics.html#intercepting-emails
    [Recipient Interceptor]: https://github.com/croaky/recipient_interceptor
    [default_url_options]: https://guides.rubyonrails.org/action_mailer_basics.html#generating-urls-in-action-mailer-views
    [default configuration]: https://guides.rubyonrails.org/configuring.html#config-action-mailer-delivery-method

---
+++ lib/generators/suspenders/email_generator.rb
@@ -0,0 +1,52 @@
+module Suspenders
+  module Generators
+    class EmailGenerator < Rails::Generators::Base
+      source_root File.expand_path("../../templates/email", __FILE__)
+      desc <<~TEXT
+        Intercepts emails in non-production environments by setting `INTERCEPTOR_ADDRESSES`.
+
+        ```sh
+        INTERCEPTOR_ADDRESSES="user_1@example.com,user_2@example.com" bin/rails s
+        ```
+
+        Configures `default_url_options` in `test` and `development`.
+      TEXT
+
+      def create_email_interceptor
+        copy_file "email_interceptor.rb", "app/mailers/email_interceptor.rb"
+      end
+
+      def create_email_interceptor_initializer
+        initializer "email_interceptor.rb", <<~RUBY
+          Rails.application.configure do
+            if ENV["INTERCEPTOR_ADDRESSES"].present?
+              config.action_mailer.interceptors = %w[EmailInterceptor]
+            end
+          end
+        RUBY
+      end
+
+      def configure_email_interceptor
+        environment do
+          %(
+            config.to_prepare do
+              EmailInterceptor.config.interceptor_addresses = ENV.fetch("INTERCEPTOR_ADDRESSES", "").split(",")
+            end
+          )
+        end
+      end
+
+      # TODO: Remove once https://github.com/rails/rails/pull/51191 is in latest
+      # Rails release
+      def configure_development_environment
+        environment %(config.action_mailer.default_url_options = { host: "localhost", port: 3000 }), env: "development"
+      end
+
+      # TODO: Remove once https://github.com/rails/rails/pull/51191 is in latest
+      # Rails release
+      def configure_test_environment
+        environment %(config.action_mailer.default_url_options = { host: "www.example.com" }), env: "test"
+      end
+    end
+  end
+end
