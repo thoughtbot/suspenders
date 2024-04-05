5a733e0abe15db918ac4a7f973d19e22b19742cb
Steve Polito <stevepolitodesign@users.noreply.github.com>
2024-04-05 13:36:47 -0400

    Introduce `suspenders:test:environment` generator (#1182)
    Configures test environment. This differs from #1156 in that this commit
    is concerned with configuration, where that commit was concerned with
    generating a holistic test suite. It's also possible to run each
    generator independently, and the two should not rely on one another.
    
    Disables [action_dispatch.show_exceptions][] in an effort to [improve
    failure output][comment].
    
    Enables [raise_on_missing_translations][] to keep parity with the same
    setting in development #1149
    
    [action_dispatch.show_exceptions]: https://edgeguides.rubyonrails.org/configuring.html#config-action-dispatch-show-exceptions
    [comment]: https://github.com/thoughtbot/suspenders/pull/1149#discussion_r1424458925
    [raise_on_missing_translations]: https://guides.rubyonrails.org/configuring.html#config-i18n-raise-on-missing-translations

---
+++ test/generators/suspenders/test/environment_generator_test.rb
@@ -0,0 +1,69 @@
+require "test_helper"
+require "generators/suspenders/test/environment_generator"
+
+module Suspenders
+  module Generators
+    module Test
+      class EnvironmentGeneratorTest < Rails::Generators::TestCase
+        include Suspenders::TestHelpers
+
+        tests Suspenders::Generators::Test::EnvironmentGenerator
+        destination Rails.root
+        setup :prepare_destination
+        teardown :restore_destination
+
+        test "raise on missing translations" do
+          run_generator
+
+          assert_file app_root("config/environments/test.rb") do |file|
+            assert_match(/^\s*config\.i18n\.raise_on_missing_translations\s*=\s*true/, file)
+          end
+        end
+
+        test "raise on missing translations (when config is not commented out)" do
+          content = file_fixture("environments/test.rb").read
+          remove_file_if_exists "config/environments/test.rb"
+          touch "config/environments/test.rb", content: content
+
+          run_generator
+
+          assert_file app_root("config/environments/test.rb") do |file|
+            assert_match(/^\s*config\.i18n\.raise_on_missing_translations\s*=\s*true/, file)
+          end
+        end
+
+        test "disable action_dispatch.show_exceptions" do
+          run_generator
+
+          assert_file app_root("config/environments/test.rb") do |file|
+            assert_match(/^\s*config\.action_dispatch\.show_exceptions\s*=\s*:none/, file)
+            assert_no_match(/^\s*config\.action_dispatch\.show_exceptions\s*=\s*:rescuable/, file)
+            assert_no_match(/^\s*#\s*Raise exceptions instead of rendering exception templates/i, file)
+          end
+        end
+
+        test "disable action_dispatch.show_exceptions (when config does not exist)" do
+          content = file_fixture("environments/test.rb").read
+          remove_file_if_exists "config/environments/test.rb"
+          touch "config/environments/test.rb", content: content
+
+          run_generator
+
+          assert_file app_root("config/environments/test.rb") do |file|
+            assert_match(/^\s*config\.action_dispatch\.show_exceptions\s*=\s*:none/, file)
+          end
+        end
+
+        private
+
+        def prepare_destination
+          backup_file "config/environments/test.rb"
+        end
+
+        def restore_destination
+          restore_file "config/environments/test.rb"
+        end
+      end
+    end
+  end
+end
