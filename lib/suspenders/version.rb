117325b2a97e04f72743c811177c66d0e32221e9
Nick Charlton <nick@nickcharlton.net>
2024-11-28 14:47:52 +0000

    Add support for Rails 8.0

--- lib/suspenders/version.rb
+++ lib/suspenders/version.rb
@@ -1,6 +1,6 @@
 module Suspenders
   VERSION = "20240516.0".freeze
-  RAILS_VERSION = "~> 7.0".freeze
+  RAILS_VERSION = "~> 8.0".freeze
   MINIMUM_RUBY_VERSION = ">= 3.1".freeze
   MINIMUM_NODE_VERSION = "20.0.0".freeze
 end
